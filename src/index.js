549cb1b848084a44b434ed48703269ef4c4b84d0
KumarYash28 <kumyash2811@gmail.com>
2024-07-22 18:38:50 +0530

    final commigt

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1><u><center>TEXT EDITOR</center></u></h1>
     <App />
   </React.StrictMode>
 );
